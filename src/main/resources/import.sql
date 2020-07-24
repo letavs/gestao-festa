46c390f51a9e52f1af18147401674c98a47200da
letavs <letavs@V7>
2020-07-24 14:15:31 -0300

    organizacao do projeto

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES(1, 'Pedro', 2)
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES(2, 'Leandro', 7)
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES(3, 'Vinicius', 3)
