f055755446c88fcd3a5aa8ca9edacec737ea395a
Leandro Tavares <letavs@gmail.com>
2020-07-26 00:09:51 -0300

    update

--- src/main/java/com/algawork/festa/GestaoFestaApplication.java
+++ src/main/java/com/algawork/festa/GestaoFestaApplication.java
@@ -1,15 +1,8 @@
 package com.algawork.festa;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.algawork.festa.model.Convidado;
-import com.algawork.festa.model.Convidados;
 
 @SpringBootApplication
 public class GestaoFestaApplication {
@@ -21,8 +14,5 @@ public class GestaoFestaApplication {
 	String index() {
 		return "index";
 	}
-	@RequestMapping("/convidados")
-	String lista() {
-		return "ListaConvidados";
-	}
+
 }
