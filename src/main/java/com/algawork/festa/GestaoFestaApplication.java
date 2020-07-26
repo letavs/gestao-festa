053575f3b367ec62faed7087315d6a54f47b9270
Leandro Tavares <letavs@gmail.com>
2020-07-25 23:56:53 -0300

    Movido metodosRquestMapping(/convidados) para classe principal

--- src/main/java/com/algawork/festa/GestaoFestaApplication.java
+++ src/main/java/com/algawork/festa/GestaoFestaApplication.java
@@ -1,8 +1,15 @@
 package com.algawork.festa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algawork.festa.model.Convidado;
+import com.algawork.festa.model.Convidados;
 
 @SpringBootApplication
 public class GestaoFestaApplication {
@@ -13,6 +20,24 @@ public class GestaoFestaApplication {
 	@RequestMapping("/")
 	String index() {
 		return "index";
+	}
+	@Autowired
+	private Convidados convidados;
+
+	@GetMapping
+	@RequestMapping("/convidados")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject("convidados", convidados.findAll());
+		modelAndView.addObject(new Convidado());
+		return modelAndView;
+	}
+
+	@PostMapping
+	@RequestMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 
 }
